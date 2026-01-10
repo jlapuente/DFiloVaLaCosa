1d686fdb4dc1cf5d81ac118ae80b8460024c3ba6
jlapuente <javierlapuentegarcia@gmail.com>
2026-01-10 15:54:29 +0100

    Deleted vendor JSs

--- src/app/lib/post-skeleton/post-skeleton.component.ts
+++ src/app/lib/post-skeleton/post-skeleton.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-post-skeleton',
   templateUrl: './post-skeleton.component.html',
-  styleUrls: ['./post-skeleton.component.css']
+  styleUrls: ['./post-skeleton.component.scss']
 })
 export class PostSkeletonComponent {
 
