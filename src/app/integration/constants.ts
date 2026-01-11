07184af81c46e602ea87d1ac6b96bbf7107619d8
jlapuente <javierlapuentegarcia@gmail.com>
2026-01-11 14:45:40 +0100

    Eliminar docs y constants.ts del historial

--- src/app/integration/constants.ts
+++
@@ -1,6 +0,0 @@
-export class Constants {
-  public static SERVICE_ID: string = "service_i3d4iym";
-  public static TEMPLATE_ID: string = "template_9krvvjk";
-  public static PUBLIC_KEY: string = "otX17U7dVVPumk7Cw";
-  public static CONTENTFUL_ACCESS_TOKEN = "W06kssg7ZcRcTsPdb1uo7T4T82QpnUdYklZMqOINalI";
-}
